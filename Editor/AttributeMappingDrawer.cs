1d80cea4732c32f32ca9f134cffebbf0f83d0f54
Chris Hill <chris@enemyhideout.com>
2020-03-23 13:53:29 -0700

    Adding property drawers for paths and attributes.

---
+++ Editor/AttributeMappingDrawer.cs
@@ -0,0 +1,53 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace com.enemyhideout.retargeting
+{
+    // AttributeMappingDrawer
+    [CustomPropertyDrawer(typeof(AttributeMapping))]
+    public class AttributeMappingDrawer : PropertyDrawer
+    {
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            SerializedProperty actionProp = property.FindPropertyRelative("action");
+            float actionControlWidth = 100;
+            bool isDeleting = (AttributeMappingAction)actionProp.intValue == AttributeMappingAction.Delete;
+            float controlSize = isDeleting ? position.width - actionControlWidth : (position.width - actionControlWidth) * 0.5f;
+            // Calculate rects
+            var fromRect = new Rect(position.x, position.y, controlSize, position.height);
+            var toRect = new Rect(position.x + controlSize, position.y, controlSize, position.height);
+            var actionRect = new Rect(position.x + controlSize*2, position.y, actionControlWidth, position.height);
+            if(isDeleting)
+            {
+                actionRect.x = position.x + controlSize;
+            }
+
+            EditorGUIUtility.labelWidth = 40;
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.PropertyField(fromRect, property.FindPropertyRelative("fromPath"), new GUIContent("From"));
+            if(!isDeleting)
+            {
+                EditorGUI.PropertyField(toRect, property.FindPropertyRelative("toPath"), new GUIContent("To"));
+            }
+            EditorGUI.PropertyField(actionRect, actionProp, GUIContent.none);
+            EditorGUIUtility.labelWidth = 0;
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Editor/PathMappingDrawer.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace com.enemyhideout.retargeting
+{
+    // PathMappingDrawer
+    [CustomPropertyDrawer(typeof(PathMapping))]
+    public class PathMappingDrawer : PropertyDrawer
+    {
+        // Draw the property inside the given rect
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            float matchTypeControlWidth = 100;
+            float controlSize = (position.width - matchTypeControlWidth) * 0.5f;
+            // Calculate rects
+            var fromRect = new Rect(position.x, position.y, controlSize, position.height);
+            var toRect = new Rect(position.x + controlSize, position.y, controlSize, position.height);
+            var matchTypeRect = new Rect(position.x + controlSize*2, position.y, matchTypeControlWidth, position.height);
+
+            EditorGUIUtility.labelWidth = 40;
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.PropertyField(fromRect, property.FindPropertyRelative("fromPath"), new GUIContent("From"));
+            EditorGUI.PropertyField(toRect, property.FindPropertyRelative("toPath"), new GUIContent("To"));
+            EditorGUI.PropertyField(matchTypeRect, property.FindPropertyRelative("matchType"), GUIContent.none);
+            EditorGUIUtility.labelWidth = 0;
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+    }
+}
